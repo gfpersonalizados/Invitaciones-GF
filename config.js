4547660b609f2bae70d2ee7f60ec53b406674bb6
gfpersonalizados <sperez.sg84@gmail.com>
2026-07-20 15:40:01 -0600

    Create config.js with event configuration
    Added configuration settings for event details including names, date, location, contact, and media.

---
+++ config.js
@@ -0,0 +1,23 @@
+const CONFIG = {
+  novios: "Michel & Josue",
+
+  fecha: "2026-12-27T15:00:00",
+
+  ceremonia: "Parroquia San Francisco de Asís, Yucuaiquín",
+
+  recepcion: "Ranchón de la Hacienda",
+
+  whatsapp: "50377777777",
+
+  maps: "https://www.google.com/maps/place/Ranch%C3%B3n+de+La+Hacienda/@13.5241992,-88.0046338,17z/data=!3m1!4b1!4m6!3m5!1s0x8f7ad30032da1b41:0x4dc42d74667433bd!8m2!3d13.5241992!4d-88.0020589!16s%2Fg%2F11vk3s0ysw",
+
+  video: "invitacion.mp4",
+
+  musica: "musica.mp3",
+
+  fotos: [
+    "foto1.jpg",
+    "foto2.jpg",
+    "foto3.jpg"
+  ]
+};
