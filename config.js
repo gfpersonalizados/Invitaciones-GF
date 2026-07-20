45aaf8e44a9fb0bbc7ad6f0a759e946f2fc850ad
gfpersonalizados <sperez.sg84@gmail.com>
2026-07-20 17:06:54 -0600

    Update wedding event details in config.js

--- config.js
+++ config.js
@@ -4,55 +4,75 @@ const CONFIG = {
     // NOVIOS
     //===========================
 
-    novio: "Carlos",
-
-    novia: "María",
-
+    novio: "Josue",
+    novia: "Michell",
 
     //===========================
-    // FECHAS
+    // FECHA
     //===========================
 
-    fechaTexto: "14 de febrero de 2027",
+    fechaTexto: "27 de Diciembre de 2026",
+    fechaCuenta: "2026-12-27 15:00:00",
 
-    // Formato:
-    // Año-Mes-Día Hora:Minuto:Segundo
+    //===========================
+    // HISTORIA
+    //===========================
 
-    fechaCuenta: "2027-02-14 16:00:00",
+    historia: "Después de muchos momentos compartidos, llegó el día de dar el siguiente paso. Queremos celebrar este momento tan especial junto a ustedes.",
 
+    //===========================
+    // VIDEO (YouTube)
+    //===========================
+
+    video: "https://www.youtube.com/embed/VIDEO_ID",
 
     //===========================
     // CEREMONIA
     //===========================
 
-    ceremonia:
-    "Iglesia El Rosario<br>4:00 PM",
+    ceremonia: {
 
+        nombre: "Parroquia San Francisco de Asis",
+
+        direccion: "Yucuaiquin, La Union Sur",
+
+        mapa: "https://www.google.com/maps/place/Iglesia+Parroquial+San+Francisco+de+As%C3%ADs/@13.5483078,-88.0040033,17z/data=!3m1!4b1!4m6!3m5!1s0x8f6532baba74a0c5:0xcea582cf348da68f!8m2!3d13.5483078!4d-88.0014284!16s%2Fg%2F1hf3jqrrp?entry=ttu&g_ep=EgoyMDI2MDcxNS4wIKXMDSoASAFQAw%3D%3D"
+
+    },
 
     //===========================
     // RECEPCIÓN
     //===========================
 
-    recepcion:
-    "Hotel Real Intercontinental<br>6:00 PM",
+    recepcion: {
 
+        nombre: "Ranchon de La Hacienda",
+
+        direccion: "colonia las brisas, cacerio palo galan, Yucuaiquin",
+
+        mapa: "https://www.google.com/maps/place/Ranch%C3%B3n+de+La+Hacienda/@13.5241992,-88.0046338,17z/data=!3m1!4b1!4m6!3m5!1s0x8f7ad30032da1b41:0x4dc42d74667433bd!8m2!3d13.5241992!4d-88.0020589!16s%2Fg%2F11vk3s0ysw?entry=ttu&g_ep=EgoyMDI2MDcxNS4wIKXMDSoASAFQAw%3D%3D"
+
+    },
 
     //===========================
-    // MAPA
+    // GALERÍA
     //===========================
 
-    mapa:
-    "https://maps.google.com/",
+    galeria: [
 
+        "img/foto1.jpg",
+        "img/foto2.jpg",
+        "img/foto3.jpg",
+        "img/foto4.jpg",
+        "img/foto5.jpg"
+
+    ],
 
     //===========================
     // WHATSAPP
     //===========================
 
-    telefono:
-    "50370000000",
-
-    mensajeWhatsapp:
-    "Hola, confirmo mi asistencia a la boda ❤️"
+    whatsapp:
+    "https://wa.me/50373062031?text=Hola%2C%20confirmo%20mi%20asistencia%20a%20la%20boda%20❤️"
 
 };
