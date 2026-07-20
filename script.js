2c5e4273c780d35fbb7a5c5d823a07d995af3ebe
gfpersonalizados <sperez.sg84@gmail.com>
2026-07-20 15:19:53 -0600

    Refactor script.js to remove function and add CONFIG
    Removed abrirInvitacion function and added CONFIG object with event details.

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-function abrirInvitacion() {
-
-    const video = document.querySelector("video");
-
-    video.scrollIntoView({
-        behavior: "smooth"
-    });
-
-    setTimeout(() => {
-
-        video.play();
-
-        if (video.requestFullscreen) {
-            video.requestFullscreen();
-        }
-
-    }, 700);
-
-}
+const CONFIG = {
+  novios: "Ana & Carlos",
+  fecha: "2026-12-20T16:00:00",
+  ceremonia: "Iglesia El Rosario",
+  recepcion: "Hotel Real",
+  whatsapp: "50377777777",
+  maps: "https://maps.app.goo.gl/",
+  video: "Invitacion.mp4",
+  musica: "musica.mp3",
+  fotos: [
+    "foto1.jpg",
+    "foto2.jpg",
+    "foto3.jpg"
+  ]
+};
